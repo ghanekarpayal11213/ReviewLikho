01f3b1253b829bc25161cb37a3561913538f2a82
ghanekarpayal11213 <126890237+ghanekarpayal11213@users.noreply.github.com>
2025-07-20 19:59:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,99 +1,58 @@
 import './App.css'
-import {  Route} from 'react-router-dom';
 import Navbar from './components/Navbar.js'
 import ProductList from './components/ProductList.js'
 import Footer from './components/Footer.js'
 import React, { useState } from 'react';
 import AddItem from './components/AddItem.js';
-import HomePage from './components/Homepage.js';
-
 
 function App() {
-  var productList = [
+  const initialProductList = [
     {
-      reviews: 'Your approch to capture moments is very nice. Thank you!  ',
+      reviews: 'Your approach to capture moments is very nice. Thank you!',
       name: 'Rakhi Mandloi',
-    
     },
     {
       reviews: 'You have done great Photography!',
       name: 'Virat Kohli',
-    
     },
-  ]
-  var [productList, setProductList] = useState(productList)
-  let [TotalAmount, setTotalAmount] = useState(0)
+  ];
 
-  //const increamentQuantity = (index) => {
-  //let newProductList = [...productList]
-   // let newTotalAmount = TotalAmount
-    //newProductList[index].quantity++
-    //newTotalAmount += newProductList[index].price
-    //setTotalAmount(newTotalAmount)
-    //setProductList(newProductList)
-  //}
+  const [productList, setProductList] = useState(initialProductList);
+  const [TotalAmount, setTotalAmount] = useState(0);
 
-  //const decreamentQuantity = (index) => {
-    //let newProductList = [...productList]
-    //let newTotalAmount = TotalAmount
-    //if (newProductList[index].quantity > 0) {
-      //newProductList[index].quantity--
-      //newTotalAmount -= newProductList[index].price
-    //}
-    //setTotalAmount(newTotalAmount)
-    //setProductList(newProductList)
-  //}
+  const removeItem = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = TotalAmount;
+    newTotalAmount -= (newProductList[index].quantity || 0) * (newProductList[index].price || 0);
+    newProductList.splice(index, 1);
+    setProductList(newProductList);
+    setTotalAmount(newTotalAmount);
+  }
 
-  //const resetQuantity= () =>{
-    //let newProductList = [...productList]
-    //newProductList.map((products)=>{
-    //products.quantity=0
-     //})
-  //setProductList(newProductList)
-  //setTotalAmount(0)
-//}
-
-const removeItem =(index) =>{
-  let newProductList = [...productList]
-  let newTotalAmount = TotalAmount
-  newTotalAmount -= 
-  newProductList[index].quantity * newProductList[index].price;
-  newProductList.splice(index,1);
-  setProductList(newProductList)
-  setTotalAmount(newTotalAmount)
-}
-
-const addItem = (name,reviews)=>{
-  let newProductList = [...productList]
-  newProductList.push({
-  reviews:reviews,
-  name:name,
-  quantity:0
-  })
-  setProductList(newProductList)
-}
+  const addItem = (name, reviews) => {
+    let newProductList = [...productList];
+    newProductList.push({
+      reviews: reviews,
+      name: name,
+      quantity: 0,
+      price: 0
+    });
+    setProductList(newProductList);
+  }
 
   return (
     <>
       <Navbar />
       <main className="container mt-5">
-        <AddItem addItem={addItem}/>
+        <AddItem addItem={addItem} />
         <ProductList
           productList={productList}
-          //increamentQuantity={increamentQuantity}
-          //decreamentQuantity={decreamentQuantity}
           removeItem={removeItem}
         />
       </main>
       <Footer TotalAmount={TotalAmount} />
     </>
-  )
-}
-
-const HomePagecon = () => {
-  return (
-        <Route exact path="/" component={HomePage} />
   );
-};
+}
 
 export default App;
